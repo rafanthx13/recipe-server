0400e7b6757dba0c0d7fadbb2feb67d02cbcd8cb
Rafael Assis <rafaassis15@gmail.com>
2020-05-21 00:24:26 -0300

    First Commit

---
+++ src/main/java/br/rafanthx13/recipesserver/RecipesServerApplication.java
@@ -0,0 +1,13 @@
+package br.rafanthx13.recipesserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/rafanthx13/recipesserver/RecipesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.rafanthx13.recipesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
