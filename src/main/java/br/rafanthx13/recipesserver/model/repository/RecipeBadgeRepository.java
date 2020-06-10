f50c460fdad67ff30372dca13bfa89c9482c95fc
Rafael Assis <rafaassis15@gmail.com>
2020-06-10 16:29:26 -0300

    To Deploy

--- src/main/java/br/rafanthx13/recipesserver/model/repository/RecipeBadgeRepository.java
+++ src/main/java/br/rafanthx13/recipesserver/model/repository/RecipeBadgeRepository.java
@@ -15,11 +15,11 @@ public interface RecipeBadgeRepository extends JpaRepository<RecipeBadge, Long> 
 //    @Query("SELECT t FROM Todo t where t.title = ?1 AND t.description = ?2")
     // public Optional<RecipeBadge> findByRecipe_idAndBadge_id(Long recipe_id, Long badge_id);
     @Modifying
-    @Query(value = "delete from recipe_badge rb where rb.recipe_id=:recipe_id", nativeQuery = true)
+    @Query(value = "delete from recipe_badge where recipe_id = :recipe_id", nativeQuery = true)
     public int deleteByRecipe_id(@Param("recipe_id") Long recipe_id);
 
 //    @Modifying
-    @Query(value = "select * from recipe_badge rb where rb.recipe_id=:recipe_id", nativeQuery = true)
+    @Query(value = "select * from recipe_badge rb where rb.recipe_id = :recipe_id", nativeQuery = true)
     List<RecipeBadge> findReByRecipeId(@Param("recipe_id") Long recipe_id);
 
 //     Long deleteByRecipe__id(Recipe recipe_id);
